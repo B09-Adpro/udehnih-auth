f55ac693638f3653ec33a9f1169addc75ad6ad7f
gracekarinn <gracekarina12@gmail.com>
2025-04-03 17:29:23 +0700

    init

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/UdehnihAuthApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdehnihAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdehnihAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/udehnihauth/UdehnihAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdehnihAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
