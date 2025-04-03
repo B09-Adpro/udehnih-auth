bc52c8457a6e97e48c955764caacbacd04354ebb
gracekarinn <gracekarina12@gmail.com>
2025-04-04 00:34:53 +0700

    feat: add auth controller

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/controller/AuthController.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.udehnihauth.controller;
+
+import id.ac.ui.cs.advprog.udehnihauth.dto.request.LoginRequest;
+import id.ac.ui.cs.advprog.udehnihauth.dto.request.RegisterRequest;
+import id.ac.ui.cs.advprog.udehnihauth.dto.response.AuthResponse;
+import id.ac.ui.cs.advprog.udehnihauth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(
+            @RequestBody @Valid RegisterRequest request
+    ) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody @Valid LoginRequest request
+    ) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<AuthResponse> refreshToken(
+            @RequestBody String refreshToken
+    ) {
+        return ResponseEntity.ok(authService.refreshToken(refreshToken));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(
+            @RequestBody String refreshToken
+    ) {
+        authService.logout(refreshToken);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
