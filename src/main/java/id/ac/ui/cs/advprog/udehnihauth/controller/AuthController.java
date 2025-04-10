e48ce85adb37a1f654731797bee14bde7265ae78
gracekarinn <gracekarina12@gmail.com>
2025-04-11 01:32:38 +0700

    feat: add auth controller

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/controller/AuthController.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.advprog.udehnihauth.controller;
+
+import id.ac.ui.cs.advprog.udehnihauth.dto.response.AuthResponse;
+import id.ac.ui.cs.advprog.udehnihauth.dto.request.RegisterRequest;
+import id.ac.ui.cs.advprog.udehnihauth.dto.request.LoginRequest;
+import id.ac.ui.cs.advprog.udehnihauth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String authHeader) {
+        String token = null;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            authService.logout(token);
+        }
+        return ResponseEntity.ok().build();
+    }
+}
