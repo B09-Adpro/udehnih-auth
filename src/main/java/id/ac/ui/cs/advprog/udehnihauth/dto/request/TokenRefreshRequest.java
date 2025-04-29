8394d3cbfa01fe82b06e4a9ea9bbe4987e0f4aaa
gracekarinn <gracekarina12@gmail.com>
2025-04-30 00:38:20 +0700

    feat: add refresh token request

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/dto/request/TokenRefreshRequest.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.udehnihauth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRefreshRequest {
+
+    @NotBlank(message = "Refresh token is required")
+    private String refreshToken;
+}
