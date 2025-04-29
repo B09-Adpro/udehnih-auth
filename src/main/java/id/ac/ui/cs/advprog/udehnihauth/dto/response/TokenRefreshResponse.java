304c5994cf447af0873a7f5f55208753d75057fe
gracekarinn <gracekarina12@gmail.com>
2025-04-30 00:38:13 +0700

    feat: add refresh token response

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/dto/response/TokenRefreshResponse.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.udehnihauth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRefreshResponse {
+    private String accessToken;
+    private String refreshToken;
+}
