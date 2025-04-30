de098e268c797e2898bf9d335c82e5f3fcbe216d
gracekarinn <gracekarina12@gmail.com>
2025-04-30 12:36:02 +0700

    feat: add logout response

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/dto/response/LogoutResponse.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.udehnihauth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogoutResponse {
+    private String message;
+    private boolean success;
+}
