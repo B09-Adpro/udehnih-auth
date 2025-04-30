55560d01802e46db75bc87966963b420c1212dcb
gracekarinn <gracekarina12@gmail.com>
2025-04-30 12:36:46 +0700

    feat: add refresh token interface

--- src/main/java/id/ac/ui/cs/advprog/udehnihauth/service/AuthService.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/service/AuthService.java
@@ -3,9 +3,13 @@ package id.ac.ui.cs.advprog.udehnihauth.service;
 import id.ac.ui.cs.advprog.udehnihauth.dto.response.AuthResponse;
 import id.ac.ui.cs.advprog.udehnihauth.dto.request.LoginRequest;
 import id.ac.ui.cs.advprog.udehnihauth.dto.request.RegisterRequest;
+import id.ac.ui.cs.advprog.udehnihauth.dto.request.TokenRefreshRequest;
+import id.ac.ui.cs.advprog.udehnihauth.dto.response.TokenRefreshResponse;
 
 public interface AuthService {
     AuthResponse register(RegisterRequest request);
     AuthResponse login(LoginRequest request);
     void logout(String token);
+    void logout(String token, String refreshToken);
+    TokenRefreshResponse refreshToken(TokenRefreshRequest request);
 }
