b369684128fd681a4ead01bc04c7f1cd3df0c028
gracekarinn <gracekarina12@gmail.com>
2025-05-02 17:27:52 +0700

    feat: add new context

--- src/test/java/id/ac/ui/cs/advprog/udehnihauth/UdehnihAuthApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/udehnihauth/UdehnihAuthApplicationTests.java
@@ -3,13 +3,16 @@ package id.ac.ui.cs.advprog.udehnihauth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.yml")
 class UdehnihAuthApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
