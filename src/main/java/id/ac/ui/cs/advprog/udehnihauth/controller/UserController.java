b76f85e793f8e246ce31227fee2c98b09288915b
gracekarinn <gracekarina12@gmail.com>
2025-05-08 20:18:15 +0700

    feat: add user controller

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/controller/UserController.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.udehnihauth.controller;
+
+import id.ac.ui.cs.advprog.udehnihauth.dto.response.UserInfoResponse;
+import id.ac.ui.cs.advprog.udehnihauth.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserInfoResponse> getUserInfo(@PathVariable String userId) {
+        UserInfoResponse userInfo = userService.getUserInfo(userId);
+        return ResponseEntity.ok(userInfo);
+    }
+}
