570d64094f73543ae49db1d7f6d9355ba100f4dd
gracekarinn <gracekarina12@gmail.com>
2025-05-08 20:17:36 +0700

    feat: add user service implementation

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.udehnihauth.service;
+
+import id.ac.ui.cs.advprog.udehnihauth.dto.response.UserInfoResponse;
+import id.ac.ui.cs.advprog.udehnihauth.exception.UserNotFoundException;
+import id.ac.ui.cs.advprog.udehnihauth.model.User;
+import id.ac.ui.cs.advprog.udehnihauth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserInfoResponse getUserInfo(String userId) {
+        User user = userRepository.findById(Long.parseLong(userId))
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + userId));
+
+        return UserInfoResponse.builder()
+                .id(user.getId().toString())
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+    }
+}
