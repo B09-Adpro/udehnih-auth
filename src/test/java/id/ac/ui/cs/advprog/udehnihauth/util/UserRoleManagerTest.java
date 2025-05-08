77d039e10d07f1ce7aafd8f343e0838d2c126cd8
gracekarinn <gracekarina12@gmail.com>
2025-05-08 20:16:52 +0700

    feat: add user role manager test

--- src/test/java/id/ac/ui/cs/advprog/udehnihauth/util/UserRoleManagerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/udehnihauth/util/UserRoleManagerTest.java
@@ -1,5 +1,8 @@
-package id.ac.ui.cs.advprog.udehnihauth.model;
+package id.ac.ui.cs.advprog.udehnihauth.util;
 
+import id.ac.ui.cs.advprog.udehnihauth.model.Role;
+import id.ac.ui.cs.advprog.udehnihauth.model.RoleType;
+import id.ac.ui.cs.advprog.udehnihauth.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
