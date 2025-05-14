8023391dcde528fceeedc52b63778fc64e36942e
gracekarinn <gracekarina12@gmail.com>
2025-05-14 23:21:58 +0700

    feat: import cors test

--- src/test/java/id/ac/ui/cs/advprog/udehnihauth/UdehnihAuthApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/udehnihauth/UdehnihAuthApplicationTests.java
@@ -1,11 +1,14 @@
 package id.ac.ui.cs.advprog.udehnihauth;
 
+import id.ac.ui.cs.advprog.udehnihauth.config.CorsConfigTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
+@Import(CorsConfigTest.class)
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.yml")
 class UdehnihAuthApplicationTests {
