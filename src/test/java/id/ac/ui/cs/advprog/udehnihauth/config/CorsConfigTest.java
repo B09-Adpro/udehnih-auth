b4cedb5a8e11aa310018cf7ba3e174ce907945ce
gracekarinn <gracekarina12@gmail.com>
2025-05-14 23:21:42 +0700

    feat: add cors test

---
+++ src/test/java/id/ac/ui/cs/advprog/udehnihauth/config/CorsConfigTest.java
@@ -0,0 +1,30 @@
+package id.ac.ui.cs.advprog.udehnihauth.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@TestConfiguration
+@Profile("test")
+public class CorsConfigTest {
+
+    @Bean
+    @Primary
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
