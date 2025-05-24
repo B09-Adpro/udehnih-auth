8c0a3ae5152e79cedd30b5265d2a2a213da9ad44
gracekarinn <gracekarina12@gmail.com>
2025-05-24 23:35:50 +0700

    feat: add new queries to enhance performance

--- src/main/java/id/ac/ui/cs/advprog/udehnihauth/repository/RoleRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/repository/RoleRepository.java
@@ -3,6 +3,8 @@ package id.ac.ui.cs.advprog.udehnihauth.repository;
 import id.ac.ui.cs.advprog.udehnihauth.model.Role;
 import id.ac.ui.cs.advprog.udehnihauth.model.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,10 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleType name);
+
+    @Query("SELECT r FROM Role r LEFT JOIN FETCH r.users WHERE r.name = :name")
+    Optional<Role> findByNameWithUsers(@Param("name") RoleType name);
+
+    @Query("SELECT COUNT(ur) FROM User u JOIN u.roles ur WHERE ur.name = :roleName")
+    Long countUsersByRoleName(@Param("roleName") RoleType roleName);
 }
