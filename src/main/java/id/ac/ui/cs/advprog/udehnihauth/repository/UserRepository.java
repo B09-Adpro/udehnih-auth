cc493adbad15b793404e2cd10b627a214cae8d15
gracekarinn <gracekarina12@gmail.com>
2025-05-24 23:32:59 +0700

    feat: add new queries to make the loading time faster

--- src/main/java/id/ac/ui/cs/advprog/udehnihauth/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/repository/UserRepository.java
@@ -2,12 +2,27 @@ package id.ac.ui.cs.advprog.udehnihauth.repository;
 
 import id.ac.ui.cs.advprog.udehnihauth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :email")
+    Optional<User> findByEmailWithRoles(@Param("email") String email);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.id = :id")
+    Optional<User> findByIdWithRoles(@Param("id") Long id);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles r WHERE u.email = :email")
+    Optional<User> findByEmailForAuthentication(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findByIdForProfile(@Param("id") Long id);
 }
