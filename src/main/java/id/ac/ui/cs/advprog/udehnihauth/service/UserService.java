4c9653b0e8eb3e98d65e3381b9ad4acc82a5a476
gracekarinn <gracekarina12@gmail.com>
2025-05-24 23:32:42 +0700

    feat: add new interface in user service

--- src/main/java/id/ac/ui/cs/advprog/udehnihauth/service/UserService.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihauth/service/UserService.java
@@ -4,4 +4,5 @@ import id.ac.ui.cs.advprog.udehnihauth.dto.response.UserInfoResponse;
 
 public interface UserService {
     UserInfoResponse getUserInfo(Long userId);
+    UserInfoResponse getUserInfoWithRoles(Long userId);
 }
